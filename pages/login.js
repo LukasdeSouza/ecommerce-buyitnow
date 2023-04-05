9b836c9d633c3135d5bf21dcf6677f1c7d47d30c
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2023-04-05 17:01:58 -0300

    email and password validation using react-hook-form

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,52 @@
 import Layout from '@/components/Layout'
 import Link from 'next/link'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const LoginScreen = () => {
+  const { handleSubmit, register, formState: { errors } } = useForm()
+
+  const submitHandler = ({ email, password }) => {
+
+  }
+
+
   return (
     <Layout>
-      <form className='mx-auto max-w-screen-md'>
+      <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
         <h1 className='mb-4 text-xl'>Login</h1>
         <div className='mb-4'>
           <label htmlFor="email">Email</label>
-          <input type="email" className='w-full' id='email' autoFocus required />
+          <input type="email"
+            {...register('email', {
+              required: 'Digite seu email',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: 'Por favor digite um email válido',
+              },
+            })}
+            className='w-full'
+            id='email'
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
         </div>
         <div className='mb-4'>
           <label htmlFor="password">Senha</label>
-          <input type="password" className='w-full' id='password' autoFocus required />
+          <input type="password"
+            {...register('password', {
+              required: 'Digite sua senha',
+              minLength: { value: 6, message: 'Senha deve ser maior do que 5 caractéres' },
+            })}
+            className='w-full'
+            id='password'
+            autoFocus
+          />
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
         </div>
         <div className='mb-4'>
           <button className='rounded bg-amber-300 py-2 mt-2 px-3 shadow outline-none hover:bg-amber-400 active:bg-amber-500 font-semibold w-1/5'>
