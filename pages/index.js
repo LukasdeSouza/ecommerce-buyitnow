88ef355b5d839cbc430e2b5ce8d00a97dd2709aa
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2023-04-06 16:03:30 -0300

    fixing header

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <Layout title={'Home'}>
+      <Layout title={'Home |'}>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
           {data.products.map((product) => (
             <ProductItem product={product} key={product.slug}></ProductItem>
